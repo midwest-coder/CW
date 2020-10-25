5c351261c83abce71e5cbdf4a2dcd65e36235cfc
Kevin <kstamps2484@gmail.com>
2020-10-25 14:31:18 -0600

    Updates

---
+++ precache-manifest.5ada802e8ac9a987302dc6b4b3713766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "645f537ffdfee0eb33f1",
+    "url": "/CW/static/css/main.43921a4a.chunk.css"
+  },
+  {
+    "revision": "645f537ffdfee0eb33f1",
+    "url": "/CW/static/js/main.645f537f.chunk.js"
+  },
+  {
+    "revision": "fb8ed7c56d872572c0d6",
+    "url": "/CW/static/css/1.67de8542.chunk.css"
+  },
+  {
+    "revision": "fb8ed7c56d872572c0d6",
+    "url": "/CW/static/js/1.fb8ed7c5.chunk.js"
+  },
+  {
+    "revision": "5208a1836f0eec8ff227",
+    "url": "/CW/static/js/runtime~main.5208a183.js"
+  },
+  {
+    "revision": "a66c6118260fd33644d1e0b737891b5f",
+    "url": "/CW/static/media/CW_BG.a66c6118.png"
+  },
+  {
+    "revision": "8c037b51f1cffbcd7c5c2f8c8c562122",
+    "url": "/CW/index.html"
+  }
+];
